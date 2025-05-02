f322df317ee840cff003fe0c366c86f962809068
Ernest326 <ernest326648@gmail.com>
2025-05-02 12:41:06 +0100

    Bug fixes, error handling, separate thread for scraper to prevent UI from freezing. More user friendly.

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import tkinter as tk
 from tkinter import filedialog, messagebox
 import requests
 import scraper
+import re
+import threading
+from time import sleep
 
 root = tk.Tk()
 root.title("Maynooth Paper Scraper")
@@ -9,17 +12,44 @@ root.geometry("640x400")
 root.resizable(False, False)
 
 output_folder = "./papers"  # Default output folder
+scraper = scraper.Scraper()
 # -----------------------------------------------------------------------
 
-def start_scraper():
-    result = scraper.scrape(user.get(), password.get(), module.get(), output_folder)
+def run_scraper_thread():
+    result = scraper.start(user.get(), password.get(), module.get().upper(), output_folder)
     if result != True:
         messagebox.showerror("Error", result)
     else:
         print("Generating Notes...")
         messagebox.showinfo("Success", "Scraping completed successfully!")
+        start_button.config(state="normal")
+        
+    start_button.config(state="normal", text="Start")
+    start_button.update_idletasks()
+
+def start_scraper():
+    
+    # Error handling for empty fields
+    if re.search("[0-9]{8}", user.get()) is None:
+        messagebox.showerror("Error", "Invalid username format. Use your student ID (e.g., 2345678)")
+        return
+    if not password.get():
+        messagebox.showerror("Error", "Password cannot be empty")
+        return
+    if not module.get():
+        messagebox.showerror("Error", "Module code cannot be empty")
+        return
+    if not output_folder:
+        messagebox.showerror("Error", "Output folder cannot be empty")
+        return
+
+    # Start the scaper
+    start_button.config(state="disabled", text="Scraping...")
+    start_button.update_idletasks()
+    threading.Thread(target=run_scraper_thread).start()
 
 def select_output_folder():
+    
     global output_folder
     folder_path = filedialog.askdirectory(title="Select Output Folder")
     if folder_path:
@@ -55,7 +85,7 @@ output_label = tk.Label(paper_frame, text="./papers")
 output_label.grid(row=1, column=1, padx=5, pady=5, sticky="w")
 
 # AI SETTINGS
-ai_frame = tk.LabelFrame(root, text="AI Settings", padx=10, pady=10)
+ai_frame = tk.LabelFrame(root, text="AI Settings (Note generation currently unavailable)", padx=10, pady=10)
 ai_frame.pack(fill="both", padx=10, pady=10)
 tk.Label(ai_frame, text="GPT API Key:").grid(row=0, column=0, padx=5, pady=5, sticky="w")
 tk.Label(ai_frame, text="Options:").grid(row=1, column=0, padx=5, pady=5, sticky="w")
